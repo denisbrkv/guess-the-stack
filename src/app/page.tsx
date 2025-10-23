25cabac14a62e75be5ccc01d1a604f731e38a1f6
DenisBrkv <buryakovdv@gmail.com>
2025-10-24 00:17:50 +0400

    add bg images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>123</div>;
+import { Logo } from "@/shared/ui/logo";
+
+import styles from "./page.module.css";
+
+export default function Main() {
+  return (
+    <div className={styles.inner}>
+      <Logo className={styles.logo} />
+      <h1>Угадай стеки</h1>
+    </div>
+  );
 }
