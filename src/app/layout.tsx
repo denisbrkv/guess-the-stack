1c0d79f4ee4b154e5cc72c2bf599d94dbcdceb59
DenisBrkv <buryakovdv@gmail.com>
2025-10-28 19:03:25 +0400

    add meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,27 @@ import "./styles/index.css";
 export const metadata: Metadata = {
   title: "Угадай стеки - Звук",
   description: "Угадай стеки - Звук",
+  openGraph: {
+    title: "Угадай стеки — Звук",
+    description: "Угадай стеки - Звук",
+    siteName: "Угадай стеки - Звук",
+    images: [
+      {
+        url: "/icon.png",
+        width: 512,
+        height: 512,
+        alt: "Угадай стеки — Звук",
+      },
+    ],
+    locale: "ru_RU",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Угадай стеки — Звук",
+    description: "Угадай стеки — Звук",
+    images: ["/icon.png"],
+  },
 };
 
 export default function RootLayout({
